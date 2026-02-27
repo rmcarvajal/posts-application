424a1d5647a874bf2f5e2a8669963f0fac9b1b8a
rmcarvajal <darwyn089@gmail.com>
2026-02-26 23:57:30 -0500

    added fetch function

---
+++ src/components/Form.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+
+export const Form = () => {
+const [data,setData] = useState([])
+
+    useEffect(() => {
+    const getAPI = async () => {
+        try {
+            const response = await fetch("https://users-api-sable.vercel.app/api/users")
+            .then((res) => res.json()
+        .then((users) => setData(users)))
+        } catch (error){
+            console.log(error)
+        }
+    }; getAPI()
+},[])
+
+
+    return(
+        <>
+        <h1>Please fill out the form</h1>
+        <input type="text" placeholder="Insert a title"/>
+        <input type="text" placeholder="Insert description"/>
+        <input type="text" placeholder="Insert image link"/>
+        <button >Submit</button>
+        <div>
+            {data.map((info)=> (
+                <h2>{info.name}</h2>
+            ))}
+        </div>
+        </>
+    )
+}
