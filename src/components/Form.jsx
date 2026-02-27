573947d806221b91c6f6ad4fac70977028008a75
rmcarvajal <darwyn089@gmail.com>
2026-02-27 01:30:18 -0500

    post method working

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,6 +3,9 @@ import { useState, useEffect } from "react";
 
 export const Form = () => {
 const [data,setData] = useState([])
+const [title,setTitle] = useState("")
+const [desc, setDesc] = useState("")
+const [image, setImage] = useState("")
 
     useEffect(() => {
     const getAPI = async () => {
@@ -16,14 +19,31 @@ const [data,setData] = useState([])
     }; getAPI()
 },[])
 
+const post = async () => {
+    try{
+            const response = await fetch("https://users-api-sable.vercel.app/api/users",{
+                method: "POST",
+                headers: {"Content-Type":"application/json"},
+                body: JSON.stringify(
+                    {
+                        name: title,
+                        email: desc,
+                        image: image
+                    }
+                )
+            })
+    } catch (error){
+        console.log(error)
+    }
+}
 
     return(
         <>
         <h1>Please fill out the form</h1>
-        <input type="text" placeholder="Insert a title"/>
-        <input type="text" placeholder="Insert description"/>
-        <input type="text" placeholder="Insert image link"/>
-        <button >Submit</button>
+        <input type="text" placeholder="Insert a title" onChange={(event) => setTitle(event.target.value)}/>
+        <input type="text" placeholder="Insert description" onChange={(event) => setDesc(event.target.value)}/>
+        <input type="text" placeholder="Insert image link" onChange={(event) => setImage(event.target.value)}/>
+        <button onClick={post}>Submit</button>
         <div>
             {data.map((info)=> (
                 <div>                
